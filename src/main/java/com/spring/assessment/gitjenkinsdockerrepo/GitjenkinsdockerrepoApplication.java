beaacb577cdf505d133134d659070fa77459fd33
vigneshkanagalakshmi <vignesh.danaraj@gmail.com>
2021-07-28 17:07:07 +0530

    first commit

---
+++ src/main/java/com/spring/assessment/gitjenkinsdockerrepo/GitjenkinsdockerrepoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.assessment.gitjenkinsdockerrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitjenkinsdockerrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitjenkinsdockerrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/assessment/gitjenkinsdockerrepo/GitjenkinsdockerrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.assessment.gitjenkinsdockerrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitjenkinsdockerrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
